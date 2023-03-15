1889a0f411a81475410a28790fdfc6739d5b3fc0
Suresh <suresh.myageri.devops@gmail.com>
2023-03-15 11:55:14 -0500

    REST API

--- src/main/java/com/example/demo/controller/DemoRestController.java
+++ src/main/java/com/example/demo/controller/DemoRestController.java
@@ -10,7 +10,7 @@ public class DemoRestController {
 	@GetMapping("/demo-jenkins-docker")
 	public String getData() {
 		
-		return " Suresh: sample program running demo-jenkins-docker: Testing CI CD";
+		return " Suresh: Sample REST API to test CI CD from GIT JENKINS Docker Docker Hub";
 				
 	}	
 	
